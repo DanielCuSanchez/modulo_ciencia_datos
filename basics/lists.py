514735d3496fdf5fba9e605d7717c747ebfd8d8b
danielcu@alternet.com.mx <danielcu@alternet.com.mx>
2022-08-23 21:31:27 -0400

    Lists

---
+++ basics/lists.py
@@ -0,0 +1,13 @@
+
+newList = ["Darwin", "Migue","Pau","Esteban", 2, 4.5, True, 2, 4.5]
+
+print(type(newList))
+print(newList)
+print(len(newList))
+
+
+newList2 = list(("México", "Canada", "EUA"))
+
+print(type(newList2))
+print(newList2)
+
