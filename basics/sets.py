5846637545fa81623cd73460c0fcc699225158d5
danielcu@alternet.com.mx <danielcu@alternet.com.mx>
2022-08-24 22:30:20 -0400

    loop and methods sets

--- basics/sets.py
+++ basics/sets.py
@@ -10,3 +10,11 @@ exampleSet2 = set({"blue", "yellow", "red", 'pink', "purple"})
 print(type(exampleSet))
 print(exampleSet)
 print(exampleSet2)
+
+for element in exampleSet:
+  print(element)
+
+exampleSet.add('orange')
+exampleSet.add('DARK')
+exampleSet.add('Orange')
+print(exampleSet)
