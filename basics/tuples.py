d7b31966033cb4ab67b0b7ebd315a29be1fcab07
danielcu@alternet.com.mx <danielcu@alternet.com.mx>
2022-08-24 21:26:26 -0400

    Tupples

---
+++ basics/tuples.py
@@ -0,0 +1,21 @@
+# Tupples
+
+# They are indexed
+# They can be ordered
+# They allow duplicated values
+
+myTuple = ("Daniel", "Carlos", "Paulina", "Lorena")
+myTuple2 = tuple(("Daniel", "Carlos", "Paulina", "Lorena", "Pedro", "Daniel"))
+
+
+print(type(myTuple))
+print(type(myTuple2))
+print(len(myTuple))
+print(len(myTuple2))
+
+
+print(myTuple2)
+
+# Tupples are unchangeable
+#myTuple[0] = "Fernando"
+
