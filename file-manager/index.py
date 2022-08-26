9c9fdca051af04bcb1b969c94d79863a6ce19b8a
DanielCu <danielcu@alternet.com.mx>
2022-08-25 22:25:17 -0400

    managing files

---
+++ file-manager/index.py
@@ -0,0 +1,5 @@
+# This is how can we open a file, without any package
+myFile = open("./load.txt", "r")
+print(myFile.read())
+# the logic here is to use the method from ptyhon for files
+myFile.close()
