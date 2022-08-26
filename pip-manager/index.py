834b6ab1b2c1713e0ce359b81a1b223f45c1fb66
DanielCu <danielcu@alternet.com.mx>
2022-08-25 22:11:42 -0400

    pip manager commands

--- pip-manager/index.py
+++ pip-manager/index.py
@@ -5,3 +5,13 @@ import rand_string.rand_string as rand
 
 #here is how it works
 print(rand.RandString("ascii", 8))
+
+#For generate the file dependencies
+
+# Use: pip freeze > example.txt
+
+
+# For installing dependencies
+
+## Use: pip install -r file_name
+## Use: pip install -r example.txt
